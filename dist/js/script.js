8894b6002e3c992d5976a70d472a399cb98843ea
jongkodingabi <abisamhazim04@gmail.com>
2024-09-15 09:03:48 +0700

    set on top update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,20 @@
 // Navbar Fixed
 window.onscroll = function() {
-const header = document.querySelector('header');
-const fixedNav = header.offsetTop;
-const toTop = document.querySelector('#to-top');
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
-if(window.pageYOffset > fixedNav) {
-    header.classList.add('navbar-fixed');
-    toTop.classList.remove('hidden');
-    toTop.classList.add('flex');
-} else {
-    header.classList.remove('navbar-fixed');
-    toTop.classList.remove('flex');
-    toTop.classList.add('hidden');
-}
-}
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+};
+
 
 
 // Hamburger
